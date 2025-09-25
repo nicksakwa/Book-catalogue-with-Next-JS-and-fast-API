bc809f902a206d98b3c68fc1cfc8c665fbf69a6a
nsakwa <nsakwa@gmail.com>
2025-09-25 17:58:48 +0300

    requirements.txt installation with pip

---
+++ frontend/pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css' // Assuming a basic 'globals.css' exists
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
